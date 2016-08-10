bcd17fd754de149c2cc8ab4e08e7551710055964
Timo Kokkonen <tjko@iki.fi>
2016-08-09 21:48:45 -0700

    fix bug in reading packets when using binary protocol (thanks to Rick Watson)

--- nx-584.c
+++ nx-584.c
@@ -2,7 +2,7 @@
  *
  * Library of functions to implement NX-584 Protocol.
  * 
- * Copyright (C) 2009-2015 Timo Kokkonen <tjko@iki.fi>
+ * Copyright (C) 2009-2016 Timo Kokkonen <tjko@iki.fi>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -362,11 +362,9 @@ int nx_read_packet(int fd, nxmsg_t *msg, int protocol)
   
   /* decode message */
 
-  /* tmp[rawlen]=0; printf("tmp='%s'\n",tmp); */
-
   msg->len=msglen;
-  i=1;
-  tmpptr=tmp;
+  i=1; /* skip the message length in the buffer already */
+  tmpptr=tmp+i;
 
   while (i<=msglen+2) {
     char buf[3];
@@ -382,7 +380,7 @@ int nx_read_packet(int fd, nxmsg_t *msg, int protocol)
 	msglen=-1;
 	return -1;
       }
-    } else {
+    } else { /* NX_PROTOCOL_BINARY */
       if  (*tmpptr == 0x7e) {
 	logmsg(3,"nx_read_packet(): invalid data in packet %x (pos=%d)",*tmpptr,i);
 	len=0; /* 0x7e should always be considered as start of new packet */
